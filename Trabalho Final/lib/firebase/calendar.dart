0cfb1294246944e6e061ad525ddc5e48a4e89340
Laribene <l200805@dac.unicamp.br>
2021-01-04 22:00:21 -0300

    Remove

--- Trabalho Final/lib/firebase/calendar.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_helpers/firebase_helpers.dart';
-import 'package:trabalho_01/models/calendar_models.dart';
-
-DatabaseService<EventModel> eventDBS = DatabaseService<EventModel>("Calendario",
-    fromDS: (id, data) => EventModel.fromDS(id, data),
-    toMap: (event) => event.toMap());
--- Trabalho Final/lib/repo/auth.dart
+++
@@ -1,9 +0,0 @@
-import 'package:trabalho_01/models/user_model.dart';
-
-abstract class AuthRepo {
-  Future<UserModel> signInWithEmailAndPassword({String email, String password});
-  Future<UserModel> createUserWithEmailAndPassword(
-      {String email, String password, String nome, int role});
-  Future<void> signOut();
-  Stream<UserModel> get user;
-}
